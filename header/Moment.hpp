cffeb66e511079c145dfcba02a0e272bb428be07
TurnerZischka <tzischka7@gmail.com>
2021-02-17 07:49:40 -0800

    Created basic files for program 	1. cmakelists.txt 	2. .gitignore 	3. header and src file 	4. basic Moment.hpp file

---
+++ header/Moment.hpp
@@ -0,0 +1,24 @@
+#ifndef __MOMENT_HPP__
+#define __MOMENT_HPP__
+
+#include <string>
+
+class Moment {
+   private:
+   	double startTime;
+	double endTime;
+	double totalTime;
+
+   public:
+	string description;
+	string title;
+	string classification;
+
+
+
+
+};
+
+
+
+#endif
