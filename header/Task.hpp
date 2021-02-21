4498d4db9938b0dd619b1853fef192b99f189397
TurnerZischka <tzischka7@gmail.com>
2021-02-21 13:09:19 -0800

    Added an ID variable to the Task class

--- header/Task.hpp
+++ header/Task.hpp
@@ -9,10 +9,11 @@ class Subtask;
 class Task: public Moment {
    private:
 	vector<Subtask> sub_tasks;
+	int ID;
    public:
-	Task(string titIn, string descIn, string catIn, double startIn, double endIn, double totIn, int priIn ):Moment(titIn,descIn, catIn, startIn, endIn, totIn, priIn ){
+	Task(string titIn, string descIn, string catIn, double startIn, double endIn, double totIn, int priIn, int inID ):Moment(titIn,descIn, catIn, startIn, endIn, totIn, priIn ){
 	//do we need to initilize the sub_tasks vector?
-	
+	ID = inID;
 	}
 	void AddSubtask();
 
