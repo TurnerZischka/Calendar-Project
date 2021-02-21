7e5f8ce473149b04d17f671550d157f82f43d7ff
TurnerZischka <tzischka7@gmail.com>
2021-02-21 14:25:35 -0800

    Added test files and updated cmakelist 	1. added many test hpp files 	2. created tests.cpp 	3. updated CMakeLists.txt

---
+++ test/command_test.hpp
@@ -0,0 +1,12 @@
+#ifndef __COMMAND_TEST_HPP
+#define __COMMAND_TEST_HPP
+
+#include "gtest/gtest.h"
+
+//pls add the include for command
+
+
+
+
+
+#endif
---
+++ test/strategy_test.hpp
@@ -0,0 +1,9 @@
+#ifndef __STRATEGY_TEST_HPP__
+#define __STRATEGY_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+
+
+
+#endif
